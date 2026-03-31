50916ae9995ba76ae1876478fbe48be0d96b04a2
Arthur D'Avila <arthurdavila879@gmail.com>
2026-03-30 21:05:51 -0300

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,165 +1,190 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema universidade
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema universidade
+-- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `universidade` DEFAULT CHARACTER SET utf8mb3 ;
 USE `universidade` ;
 
-
-CREATE TABLE IF NOT EXISTS professor (
-  MatriculaProfessor INT NOT NULL,
-  Email VARCHAR(255) NOT NULL,
-
-  PRIMARY KEY (MatriculaProfessor),
-  UNIQUE INDEX Email_UNIQUE (Email),
-  
-  CONSTRAINT chk_prof_matricula 
-    CHECK (MatriculaProfessor BETWEEN 1000 AND 9999),
-
-  CONSTRAINT chk_prof_email
-    CHECK (Email LIKE '%@%.%')
-)
+-- -----------------------------------------------------
+-- Table `universidade`.`professor`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universidade`.`professor` (
+  `matriculaProfessor` INT NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `nome` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`matriculaProfessor`),
+  UNIQUE INDEX `Email_UNIQUE` (`email` ASC) VISIBLE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
-
-CREATE TABLE IF NOT EXISTS curso (
-  CodigoCurso INT NOT NULL,
-  Nome VARCHAR(45) NOT NULL,
-  CargaHoraria INT NOT NULL DEFAULT 3600,
-  Professor_Matricula INT NOT NULL,
-
-  PRIMARY KEY (CodigoCurso),
-
-  CONSTRAINT chk_curso_carga
-    CHECK (CargaHoraria >= 40),
-
-  FOREIGN KEY (Professor_Matricula)
-    REFERENCES professor (MatriculaProfessor)
-)
+-- -----------------------------------------------------
+-- Table `universidade`.`curso`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universidade`.`curso` (
+  `codigoCurso` INT NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `cargaHoraria` INT NOT NULL DEFAULT 3600,
+  `professorMatricula` INT NOT NULL,
+  PRIMARY KEY (`codigoCurso`),
+  INDEX `fk_Curso_Professor1_idx` (`professorMatricula` ASC) VISIBLE,
+  UNIQUE INDEX `Professor_Matricula_UNIQUE` (`professorMatricula` ASC) VISIBLE,
+  CONSTRAINT `fk_Curso_Professor1`
+    FOREIGN KEY (`professorMatricula`)
+    REFERENCES `universidade`.`professor` (`matriculaProfessor`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
-
-CREATE TABLE IF NOT EXISTS aluno (
-  Matricula INT NOT NULL,
-  Nome VARCHAR(45) NOT NULL,
-  Curso_Codigo INT NULL,
-
-  PRIMARY KEY (Matricula),
-
-  CONSTRAINT chk_aluno_matricula
-    CHECK (Matricula > 0),
-
-  FOREIGN KEY (Curso_Codigo)
-    REFERENCES curso (CodigoCurso)
-)
+-- -----------------------------------------------------
+-- Table `universidade`.`aluno`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universidade`.`aluno` (
+  `matricula` INT NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `curso_Codigo` INT NULL,
+  PRIMARY KEY (`matricula`),
+  INDEX `fk_Aluno_Curso1_idx` (`curso_Codigo` ASC) VISIBLE,
+  CONSTRAINT `fk_Aluno_Curso1`
+    FOREIGN KEY (`curso_Codigo`)
+    REFERENCES `universidade`.`curso` (`codigoCurso`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
-
-CREATE TABLE IF NOT EXISTS materia (
-  CodigoMateria INT NOT NULL,
-  Nome VARCHAR(45) NOT NULL,
-  CargaHorariaMin INT NOT NULL,
-
-  PRIMARY KEY (CodigoMateria),
-
-  CONSTRAINT chk_materia_carga
-    CHECK (CargaHorariaMin > 0)
-)
+-- -----------------------------------------------------
+-- Table `universidade`.`materia`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universidade`.`materia` (
+  `codigoMateria` INT NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `cargaHorariaMin` INT NOT NULL,
+  PRIMARY KEY (`codigoMateria`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
-
-CREATE TABLE IF NOT EXISTS curso_has_materia (
-  Curso_Codigo INT NOT NULL,
-  Materia_Codigo INT NOT NULL,
-
-  PRIMARY KEY (Curso_Codigo, Materia_Codigo),
-
-  FOREIGN KEY (Curso_Codigo)
-    REFERENCES curso (CodigoCurso),
-
-  FOREIGN KEY (Materia_Codigo)
-    REFERENCES materia (CodigoMateria)
-)
+-- -----------------------------------------------------
+-- Table `universidade`.`curso_has_materia`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universidade`.`curso_has_materia` (
+  `cursoCodigo` INT NOT NULL,
+  `materiaCodigo` INT NOT NULL,
+  PRIMARY KEY (`cursoCodigo`, `materiaCodigo`),
+  INDEX `fk_Curso_has_Materia_Materia1_idx` (`materiaCodigo` ASC) VISIBLE,
+  INDEX `fk_Curso_has_Materia_Curso1_idx` (`cursoCodigo` ASC) VISIBLE,
+  CONSTRAINT `fk_Curso_has_Materia_Curso1`
+    FOREIGN KEY (`cursoCodigo`)
+    REFERENCES `universidade`.`curso` (`codigoCurso`),
+  CONSTRAINT `fk_Curso_has_Materia_Materia1`
+    FOREIGN KEY (`materiaCodigo`)
+    REFERENCES `universidade`.`materia` (`codigoMateria`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
-
-CREATE TABLE IF NOT EXISTS disciplina (
-  CodigoDisciplina INT NOT NULL,
-  Vagas INT NOT NULL,
-  Semestre VARCHAR(7) NOT NULL,
-  Materia_Codigo INT NOT NULL,
-  Professor_Matricula INT NOT NULL,
-
-  PRIMARY KEY (CodigoDisciplina),
-
-  CONSTRAINT chk_disc_vagas
-    CHECK (Vagas <= 60),
-
-  CONSTRAINT chk_disc_semestre
-    CHECK (Semestre REGEXP '^[0-9]{4}\\.[12]$'),
-
-  FOREIGN KEY (Materia_Codigo)
-    REFERENCES materia (CodigoMateria),
-
-  FOREIGN KEY (Professor_Matricula)
-    REFERENCES professor (MatriculaProfessor)
-)
+-- -----------------------------------------------------
+-- Table `universidade`.`disciplina`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universidade`.`disciplina` (
+  `codigoDisciplina` INT NOT NULL,
+  `vagas` INT NOT NULL,
+  `semestre` VARCHAR(45) NOT NULL,
+  `materiaCodigo` INT NOT NULL,
+  `professorMatricula` INT NOT NULL,
+  PRIMARY KEY (`codigoDisciplina`),
+  INDEX `fk_Disciplina_Materia1_idx` (`materiaCodigo` ASC) VISIBLE,
+  INDEX `fk_Disciplina_Professor1_idx` (`professorMatricula` ASC) VISIBLE,
+  CONSTRAINT `fk_Disciplina_Materia1`
+    FOREIGN KEY (`materiaCodigo`)
+    REFERENCES `universidade`.`materia` (`codigoMateria`),
+  CONSTRAINT `fk_Disciplina_Professor1`
+    FOREIGN KEY (`professorMatricula`)
+    REFERENCES `universidade`.`professor` (`matriculaProfessor`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
-
-CREATE TABLE IF NOT EXISTS disciplina_has_aluno (
-  Disciplina_CodigoDisciplina INT NOT NULL,
-  Aluno_Matricula INT NOT NULL,
-
-  PRIMARY KEY (Disciplina_CodigoDisciplina, Aluno_Matricula),
-
-  FOREIGN KEY (Aluno_Matricula)
-    REFERENCES aluno (Matricula),
-
-  FOREIGN KEY (Disciplina_CodigoDisciplina)
-    REFERENCES disciplina (CodigoDisciplina)
-)
+-- -----------------------------------------------------
+-- Table `universidade`.`disciplina_has_aluno`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universidade`.`disciplina_has_aluno` (
+  `disciplinaCodigo` INT NOT NULL,
+  `alunoMatricula` INT NOT NULL,
+  PRIMARY KEY (`disciplinaCodigo`, `alunoMatricula`),
+  INDEX `fk_Disciplina_has_Aluno_Aluno1_idx` (`alunoMatricula` ASC) VISIBLE,
+  INDEX `fk_Disciplina_has_Aluno_Disciplina1_idx` (`disciplinaCodigo` ASC) VISIBLE,
+  CONSTRAINT `fk_Disciplina_has_Aluno_Aluno1`
+    FOREIGN KEY (`alunoMatricula`)
+    REFERENCES `universidade`.`aluno` (`matricula`),
+  CONSTRAINT `fk_Disciplina_has_Aluno_Disciplina1`
+    FOREIGN KEY (`disciplinaCodigo`)
+    REFERENCES `universidade`.`disciplina` (`codigoDisciplina`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
-
-CREATE TABLE IF NOT EXISTS emailaluno (
-  Email VARCHAR(255) NOT NULL,
-  Aluno_Matricula INT NOT NULL,
-
-  PRIMARY KEY (Aluno_Matricula, Email),
-
-  CONSTRAINT chk_emailaluno_email
-    CHECK (Email LIKE '%@%.%'),
-
-  FOREIGN KEY (Aluno_Matricula)
-    REFERENCES aluno (Matricula)
-)
+-- -----------------------------------------------------
+-- Table `universidade`.`emailaluno`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universidade`.`emailaluno` (
+  `email` VARCHAR(255) NOT NULL,
+  `alunoMatricula` INT NOT NULL,
+  PRIMARY KEY (`alunoMatricula`, `email`),
+  INDEX `fk_EmailAluno_Aluno1_idx` (`alunoMatricula` ASC) VISIBLE,
+  CONSTRAINT `fk_EmailAluno_Aluno1`
+    FOREIGN KEY (`alunoMatricula`)
+    REFERENCES `universidade`.`aluno` (`matricula`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
 
-
-CREATE TABLE IF NOT EXISTS prerequisito (
-  CodigoMateriaPre INT NOT NULL,
-  Materia_Codigo INT NOT NULL,
-
-  PRIMARY KEY (CodigoMateriaPre, Materia_Codigo),
-
-  CONSTRAINT chk_prereq_diferente
-    CHECK (CodigoMateriaPre <> Materia_Codigo),
-
-  FOREIGN KEY (Materia_Codigo)
-    REFERENCES materia (CodigoMateria)
-)
+-- -----------------------------------------------------
+-- Table `universidade`.`prerequisito`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universidade`.`prerequisito` (
+  `materiaCodigoPre` INT NOT NULL,
+  `materiaCodigo` INT NOT NULL,
+  PRIMARY KEY (`materiaCodigoPre`, `materiaCodigo`),
+  INDEX `fk_PreRequisito_Materia_idx` (`materiaCodigoPre` ASC) VISIBLE,
+  INDEX `fk_prerequisito_materia1_idx` (`materiaCodigo` ASC) VISIBLE,
+  CONSTRAINT `fk_PreRequisito_Materia`
+    FOREIGN KEY (`materiaCodigoPre`)
+    REFERENCES `universidade`.`materia` (`codigoMateria`),
+  CONSTRAINT `fk_prerequisito_materia1`
+    FOREIGN KEY (`materiaCodigo`)
+    REFERENCES `universidade`.`materia` (`codigoMateria`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+ALTER TABLE universidade.professor 
+ADD CONSTRAINT chk_matricula_digitos 
+CHECK (matriculaProfessor BETWEEN 1000 AND 9999);
+
+ALTER TABLE universidade.materia 
+ADD CONSTRAINT chk_carga_minima 
+CHECK (cargaHorariaMin >= 40);
+
+ALTER TABLE universidade.disciplina 
+ADD CONSTRAINT chk_vagas_limite 
+CHECK (vagas <= 60);
+
+ALTER TABLE universidade.professor 
+ADD CONSTRAINT chk_matricula_professor_4_digitos 
+CHECK (matriculaProfessor BETWEEN 1000 AND 9999);
